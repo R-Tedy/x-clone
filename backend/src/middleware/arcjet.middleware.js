8f23680b8c24c90ceff4d1f038727d007754421a
R-Tedy <stedyroland2@gmail.com>
2025-10-30 16:15:28 +0300

    impliment arcjet middleware.

--- backend/src/middleware/arcjet.middleware.js
+++ backend/src/middleware/arcjet.middleware.js
@@ -1,1 +1,45 @@
-import {} from "../config/arcjet.js";
+import { aj } from "../config/arcjet.js";
+
+// Arcjet middleware for rate limiting
+
+export const arcjetMiddleware = async (req, res, next) => {
+  try {
+    const decision = await aj.protect(req, {
+      requested: 1, //each request consumes 1 token
+    });
+
+    if (decision.isDenied()) {
+      if (decision.reason.isRateLimit()) {
+        return res.status(429).json({
+          error: "Too many requests",
+          message: "Rate limit exceeded. Please try again later."
+        });
+      } else if (decision.reason.isBot()) {
+        return res.status(403).json({
+          error: "Bot access denied",
+          message: "Automated requests are allowed.",
+        });
+      } else {
+        return res.status(403).json({
+          error: "Forbidden",
+          message: "Access denied by security policy."
+        });
+      }
+    }
+
+    // check for spoofed bots
+    if (decision.results.some((result) => result.res.isBot() && result.reason.isSpoofed())){
+      return res.status(403).json({
+        error: "Spoofed bot detected",
+        message: "Malicious bot activity detected.",
+      });
+    }
+
+    next();
+  } catch (error) {
+    console.error("Arcjet middleware error:", error);
+
+    // allow request to continue if Arcjet fails
+    next();
+  }
+}
