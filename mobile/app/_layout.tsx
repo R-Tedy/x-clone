3bce4d803a6f511ba7a81c6fa043bbe540678e80
R-Tedy <stedyroland2@gmail.com>
2025-10-30 20:39:29 +0300

    impliment sign in page

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,11 +1,23 @@
 import { ClerkProvider } from "@clerk/clerk-expo";
 import { tokenCache } from "@clerk/clerk-expo/token-cache";
-import { Slot } from "expo-router";
+import { Stack } from "expo-router";
+import "../global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {} from "expo-status-bar";
+import { StatusBar } from "react-native";
+
+const queryClient = new QueryClient();
 
 export default function RootLayout() {
   return (
     <ClerkProvider tokenCache={tokenCache}>
-      <Slot/>
+      <QueryClientProvider client={queryClient}>
+        <Stack screenOptions={{headerShown: false}}>
+          <Stack.Screen name="(auth)" />
+          <Stack.Screen name="(tabs)"/>
+        </Stack>
+        <StatusBar barStyle={"dark-content"}/>
+      </QueryClientProvider>
     </ClerkProvider>
   );
 }
