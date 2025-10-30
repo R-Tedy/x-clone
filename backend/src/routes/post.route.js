4fb33abcea8ace0ef0521b60028ab0a016e7d1a8
R-Tedy <stedyroland2@gmail.com>
2025-10-30 11:24:26 +0300

    impliment multer upload middleware

---
+++ backend/src/routes/post.route.js
@@ -0,0 +1,4 @@
+import express from "express";
+import {} from "../controllers/post.controller.js";
+import {} from "../middleware/auth.middleware.js";
+import upload from "../middleware/upload.middleware.js";
