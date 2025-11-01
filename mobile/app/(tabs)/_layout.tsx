f575fa5865c82812004410b3339e276295945fdb
R-Tedy <stedyroland2@gmail.com>
2025-11-01 14:11:38 +0300

    creating tab routes

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -1,5 +1,36 @@
-import { Stack } from 'expo-router/stack'
+import { Redirect, Tabs } from "expo-router";
+import { Feather } from "@expo/vector-icons";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { useAuth } from "@clerk/clerk-expo";
 
-export default function Layout() {
-  return <Stack />
+export default function TabsLayout() {
+  const insets = useSafeAreaInsets();
+
+  const {isSignedIn} = useAuth();
+
+  if (!isSignedIn) return <Redirect href={"/(auth)"}/>
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "#1da1f2",
+        tabBarInactiveTintColor: "#657786",
+        tabBarStyle: {
+          backgroundColor: "#fff",
+          borderTopWidth: 1,
+          borderTopColor: "#e1e8ed",
+          height: 50 + insets.bottom,
+          paddingTop: 8,
+        },
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "",
+          tabBarIcon: ({color, size}) => <Feather name="home" size={size} color={color} />
+        }}
+      />
+    </Tabs>
+  )
 }
